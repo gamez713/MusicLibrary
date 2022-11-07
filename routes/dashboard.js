14a22a7a8b467ded1e1a4a26cfd914ee6e707829
Tam Nguyen <tam.nguyen115@rocketmail.com>
2022-11-06 23:25:05 -0600

    database to dashboard page
    Made user playlist show up on the dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const route = express();
+const { pool } = require("../dbConfig");
 const passport = require("passport");
 const { checkNotAuthenticated } = require("../basicAuth");
 
-route.get("/dashboard", checkNotAuthenticated, (req, res) => {
-    res.render("dashboard", {user: req.user.name });
-});
 
 route.get("/uploadmusic", checkNotAuthenticated, (req, res) => {
     res.render("uploadmusic", {user: req.user.name });
