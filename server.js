5f785333e8645a70327d66e6ce742d4a7f8a5ef5
gamez713 <devedgamez@gmail.com>
2022-11-07 02:40:04 -0600

    reverting

--- server.js
+++ server.js
@@ -2,16 +2,19 @@ const initializePassport = require("./passportConfig");
 const express = require("express");
 const session = require("express-session");
 const flash = require("express-flash");
+const { checkAuthenticated } = require("./basicAuth");
+const { checkNotAuthenticated } = require("./basicAuth");
+const { pool } = require("./dbConfig");
 const passport = require("passport");
+const bcrypt = require("bcrypt");
 const app = express();
 const PORT = process.env.PORT || 4000;
-const userRoutes = require("./routes/users");
-const userDashboard = require("./routes/dashboard");
 
 initializePassport(passport);
 
 // Middlewarwe
 app.set("view engine", "ejs");
+//For CSS Files...
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended: false}));
 app.use(flash());
@@ -22,14 +25,111 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.json());
 
+// GET Directories
 app.get("/", (req, res) => {
     res.render("index");
 });
+app.get("/users/login", checkAuthenticated, (req, res) => {
+    res.render("login");
+});
+app.get("/users/register", checkAuthenticated, (req, res) => {
+    res.render("register");
+});
+app.get("/users/dashboard", checkNotAuthenticated, (req, res) => {
+    res.render("dashboard", {user: req.user.name });
+});
 
-app.use("/users", userRoutes);
-app.use("/dashboard", userDashboard);
+// /users/dashboard/userlibrary is the url link specified
+// "userlibrary" is the ejs called from views.
+app.get("/users/dashboard/userlibrary", checkNotAuthenticated, (req, res) => {
+    res.render("userlibrary");
+});
+
+app.get("/users/dashboard/uploadmusic", checkNotAuthenticated, (req, res) => {
+    res.render("uploadmusic", {user: req.user.name });
+});
+
+app.get("/users/dashboard/browse", checkNotAuthenticated, (req, res) => {
+    res.render("browse", {user: req.user.name });
+});
+
+app.get("/users/dashboard/podcast", checkNotAuthenticated, (req, res) => {
+    res.render("podcast", {user: req.user.name });
+});
+
+app.get("/users/logout", function(req, res, next) {
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      req.flash("success_msg", "Successfully logged out");
+      res.redirect("/users/login");
+    });
+});
+
+// POST
+app.post("/users/login", passport.authenticate('local', {
+    successRedirect: "/users/dashboard",
+    failureRedirect: "/users/login",
+    failureFlash: true
+}));
+
+app.post("/users/register", async (req, res) => {
+    let { name, email, password, password2 ,musician} = req.body;
+    let errors = [];
+    console.log({name, email, password, password2, musician});
+
+    // Form validation
+    if (!name || !email || !password || !password2) {
+        errors.push({ message: "All forms must be filled"});
+    }
+    if (password.length < 6) {
+        errors.push({ message: "Passwords must be at least 6 characters"});
+    }
+    if (password != password2) {
+        errors.push({ message: "Passwords do not match"});
+    }
+    if (errors.length > 0) {
+        res.render("register", { errors });
+    } else {
+        // Validation passed
+        let hashedPass = await bcrypt.hash(password, 10);
+        console.log(hashedPass);
+
+        // Checking musician boolean value
+        
+        pool.query(
+            `SELECT * FROM users
+            WHERE email = $1`,
+            [email],
+            (err, results) => {
+                if (err) {
+                    throw err;
+                }
+                console.log(results.rows);
+                // If email exists
+                if (results.rows.length > 0) {
+                    errors.push({ message: "Email already exists"});
+                    res.render("register", {errors});
+                } else {
+                    // Register new user
+                    pool.query(
+                        `INSERT INTO users (name, email, password, musician)
+                        VALUES ($1, $2, $3, $4)
+                        RETURNING id, password`, [name, email, hashedPass, musician],
+                        (err, results) => {
+                            if (err) {
+                                throw err;
+                            }
+                            console.log(results.rows)
+                            req.flash("success_msg", "Successfully registered, please log in");
+                            res.redirect("/users/login");
+                        }
+                    )
+                }
+            }
+        )
+    }
+});
 
 // Port in-use msg
 app.listen(PORT, ()=>{
