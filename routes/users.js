cbdf8bdb62f21082ebb62f703fff0ae8405e247f
Tam Nguyen <tam.nguyen115@rocketmail.com>
2022-11-10 03:03:04 -0600

    Dashboard changes.
    Shows playlist with their songs.
    Also is flexible.

--- routes/users.js
+++ routes/users.js
@@ -14,9 +14,32 @@ route.get("/register", checkAuthenticated, (req, res) => {
     res.render("register");
 });
 
-route.get("/dashboard", checkNotAuthenticated, (req, res) => {
-    res.render("dashboard", {user: req.user.fname });
+route.get("/dashboard", checkNotAuthenticated, async (req, res) => {
+    try {
+        const dict = {}
+        //counts number of playlist from specified user.
+        const playlist_count = await pool.query("SELECT COUNT(playlist.playlist_name) FROM playlist WHERE id =" + x)
+        //gets the playlist_names sorted by date created.
+        const playlist_names = await pool.query ("SELECT playlist.playlist_name FROM playlist WHERE id =" + x + "ORDER BY date_created")
+    
+        for (let b = 0; b < playlist_count.rows[0].count; b++){
+            //Song count of a playlist
+            const songs_count = await pool.query("SELECT COUNT(songs.song_name) FROM playlist, playlist_songs, songs WHERE id ="+ x + "AND playlist.playlist_id = playlist_songs.playlist_id AND songs.song_id = playlist_songs.song_id AND playlist_name ="+ "'"+ playlist_names.rows[b].playlist_name + "'" )
+            //Get the songs from each playlist
+            const songs = await pool.query("SELECT songs.song_name FROM playlist, playlist_songs, songs WHERE id ="+ x + "AND playlist.playlist_id = playlist_songs.playlist_id AND songs.song_id = playlist_songs.song_id AND playlist_name ="+ "'"+ playlist_names.rows[b].playlist_name + "'" )
+            dict[b] = [];
+            for (let g = 0; g < songs_count.rows[0].count; g++){
+                dict[b].push(songs.rows[g].song_name)
+            }
+        }
+        console.log(playlist_names.rows)
+        res.render("dashboard", {user: req.user.fname, test: dict, playlist: playlist_names.rows, pcount: playlist_count.rows[0].count});
+        } catch (e) {
+            console.log("There was an error");
+            res.send("There was an error");
+        }
 });
+
 
 route.get("/logout", function(req, res, next) {
     req.logout(function(err) {
@@ -33,12 +56,12 @@ route.post("/login", passport.authenticate('local', {
 }));
 
 route.post("/register", async (req, res) => {
-    let { fname, lname, email, password, password2 ,musician} = req.body;
+    let { name, email, password, password2 ,musician} = req.body;
     let errors = [];
-    console.log({fname, lname, email, password, password2, musician});
+    console.log({name, email, password, password2, musician});
 
     // Form validation
-    if (!fname || !email || !password || !password2) {
+    if (!name || !email || !password || !password2) {
         errors.push({ message: "All forms must be filled"});
     }
     if (password.length < 6) {
@@ -70,9 +93,9 @@ route.post("/register", async (req, res) => {
                 } else {
                     // Register new user
                     pool.query(
-                        `INSERT INTO users (fname, lname, email, password, musician)
-                        VALUES ($1, $2, $3, $4, $5)
-                        RETURNING id, password`, [fname, lname, email, hashedPass, musician],
+                        `INSERT INTO users (name, email, password, musician)
+                        VALUES ($1, $2, $3, $4)
+                        RETURNING id, password`, [name, email, hashedPass, musician],
                         (err, results) => {
                             if (err) {
                                 throw err;
