cfa0266cd97ee4404920eb65d26cbbe65a412e5f
aadproj3570 <104705067+aadproj3570@users.noreply.github.com>
2022-11-15 20:04:05 -0600

    musicplayerfix

--- routes/Musicplayer.js
+++ routes/Musicplayer.js
@@ -9,7 +9,7 @@ try {
   //counts number of playlist from specified user.
   const songs_count = await pool.query("SELECT COUNT(songs.song_name) FROM songs")
   //gets the playlist_names sorted by date created.
-  const songs = await pool.query("SELECT DISTINCT s.song_name AS name, s.artist_name AS artist, sl.song_img AS image, sl.song_link AS path FROM songs AS s, song_link as sl WHERE s.song_id = sl.song_id")
+  const songs = await pool.query("SELECT DISTINCT s.song_name AS name, s.artist_f_name AS artist, sl.song_img AS image, sl.song_link AS path FROM songs AS s, song_link as sl WHERE s.song_id = sl.song_id")
   
   //console.log(songs.rows)
   
