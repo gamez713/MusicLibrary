5a271c34e8543db02669a64673e0922fc7f3f059
Tam Nguyen <tam.nguyen115@rocketmail.com>
2022-11-27 19:19:08 -0600

    Edited Shae's Code, Remove song_time from database.
    -Removed song_time from database.
    -Fixed where song_time was used.

--- routes/Usersongs.js
+++ routes/Usersongs.js
@@ -9,7 +9,7 @@ route.get('/', checkAuth, function(req, res, next) {
     data2 = req.user.role.charAt(0).toUpperCase()+ req.user.role.slice(1);
     
     const user_id = req.user.id;
-    pool.query(`SELECT songs.song_name, songs.song_genre, songs.song_time, songs.artist_f_name, songs.artist_l_name, songbelong.id FROM songbelong
+    pool.query(`SELECT songs.song_name, songs.song_genre, songs.artist_f_name, songs.artist_l_name, songbelong.id FROM songbelong
     INNER JOIN songs
     ON songs.song_id = songbelong.song_id
     WHERE songbelong.id = $1`, [user_id],
